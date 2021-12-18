4ce5303d0058cb6bec8587f2d49c5d57560136c2
Paradis <49301263+Paradis4432@users.noreply.github.com>
2021-12-18 20:19:54 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express()
-const path = require('path')
-
-app.listen(3000,() => console.log('open '))
-
-app.use(express.static(path.resolve(__dirname, "../public")));
-
-app.use(require('./routes/web'))
--- src/routes/web.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express.Router();
-const path = require('path')
-
-app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"../views", "index.html")));
-
-module.exports= app;
